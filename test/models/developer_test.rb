59de98f555c7603a9312d10e81ad75a360ae0025
stefanoroma5 <stefanoromanini97@gmail.com>
2023-09-16 22:02:54 +0200

    added some developer test

--- test/models/developer_test.rb
+++ test/models/developer_test.rb
@@ -4,4 +4,13 @@ class DeveloperTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "developer should not be created" do
+    refute Developer.new.valid?
+  end
+
+  test "developer should be created" do
+    assert Developer.new(name: "Mario", lastname: "Rossi", email: "mario.rossi@test.com", password: "password").valid?
+  end
+
 end
