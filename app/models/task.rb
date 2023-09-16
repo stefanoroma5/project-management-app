1009deccdc98cb9edbedc7ceba1cb2902b506b60
stefanoroma5 <stefanoromanini97@gmail.com>
2023-09-16 22:02:24 +0200

    added task scopes

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,16 @@
 class Task < ApplicationRecord
   validates :description, presence: true
   validates :title, presence: true
+
+  scope :feature, -> { where(task_type: "Feature") }
+  scope :chore, -> { where(task_type: "Chore") }
+  scope :bug, -> { where(task_type: "Bug") }
+
+  scope :unstarted, -> { where(state: "Unstarted") }
+  scope :started, -> { where(state: "Started") }
+  scope :finished, -> { where(state: "Finished") }
+
+  scope :p1, -> { where(priority: "P1") }
+  scope :p2, -> { where(priority: "P2") }
+  scope :p1, -> { where(priority: "P3") }
 end
