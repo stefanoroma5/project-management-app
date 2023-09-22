65ef814a899b64ed864096292beb5790074d07f1
stefanoroma5 <stefanoromanini97@gmail.com>
2023-09-22 02:16:43 +0200

    added validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,24 @@
 class Task < ApplicationRecord
-  validates :description, presence: true
-  validates :title, presence: true
+  validates :description,
+    presence: true
+  validates :title,
+    presence: true
+  validates :end_date,
+    presence: true,
+    comparison: {greater_than: :start_date}
+  validate :start_date_cannot_be_in_the_past, :end_date_cannot_be_in_the_past
+
+  def start_date_cannot_be_in_the_past
+    if start_date.present? && start_date < Date.today
+      errors.add(:start_date, "can't be in the past")
+    end
+  end
+
+  def end_date_cannot_be_in_the_past
+    if end_date.present? && end_date < Date.today
+      errors.add(:end_date, "can't be in the past")
+    end
+  end
 
   scope :feature, -> { where(task_type: "Feature") }
   scope :chore, -> { where(task_type: "Chore") }
