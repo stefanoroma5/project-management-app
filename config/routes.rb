a6238d7e89403adcccd18621ec7f4ea5c93fba98
Alessandro Brancaleoni <alessan.brancaleoni@edu.unife.it>
2023-10-02 12:00:31 +0200

    Creata la landing page dell'applicazione

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :tasks
   resources :projects
   resources :developers
+
+  root to: "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
