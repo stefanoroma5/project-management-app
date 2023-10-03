1b3ccfdc19d24d77930b8491a0f029c118fbba30
Alessandro Brancaleoni <alessan.brancaleoni@edu.unife.it>
2023-10-03 15:43:57 +0200

    Login Making: Creato il nuovo developer con Devise; creato il login; creata la registrazione. Da collegare la pagina di sign in e sign out alla landin page; ripristinare gli attributi di developer; ripristinare le associazioni

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :developers
+
   resources :labels
   resources :tasks
   resources :projects
-  resources :developers
 
   root to: "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
