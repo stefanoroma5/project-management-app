b3bdfd2a1627a2cfddc98671e26db2752a6666d6
Alessandro Brancaleoni <alessan.brancaleoni@edu.unife.it>
2023-10-05 17:00:25 +0200

    Login Making: Completate fasi si login e registrazione

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :developers
+  devise_for :developers, :controllers => { registrations: 'developers/registrations' }
 
   resources :labels
   resources :tasks
