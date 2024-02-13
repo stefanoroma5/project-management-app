6d8daa38abac9ed95212520bb56175dcbfa5fbc2
Alessandro Brancaleoni <alessan.brancaleoni@edu.unife.it>
2024-02-13 12:11:17 +0100

    Sistemate le notice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
 
   # action per reindirizzare l'utente sulla pagina dei progetti dopo essersi loggato
   def after_sign_in_path_for(resource)
-    #redirect_to @projects
     "http://127.0.0.1:3000/projects?mode=m"
   end
 
--- app/controllers/developers/sessions_controller.rb
+++ app/controllers/developers/sessions_controller.rb
@@ -5,12 +5,19 @@ class Developers::SessionsController < Devise::SessionsController
 
   # GET /resource/sign_in
   def new
-    super
+    self.resource = resource_class.new(sign_in_params)
+    clean_up_passwords(resource)
+    yield resource if block_given?
+    respond_with(resource, serialize_options(resource))
   end
 
   # POST /resource/sign_in
   def create
-    super
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
   end
 
   # DELETE /resource/sign_out
