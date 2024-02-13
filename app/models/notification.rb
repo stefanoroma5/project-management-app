2a6a5f5fea256fea6567bab32e98e514c61b5c98
Alessandro Brancaleoni <alessan.brancaleoni@edu.unife.it>
2024-02-13 12:37:00 +0100

    Aggiunta la gemma whenever e realizzata la creazione della notifica per la scadenza delle deadlines

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,15 @@
 class Notification < ApplicationRecord
 
-    belongs_to :developer
+  belongs_to :developer
+
+  def self.check_deadlines
+    projects = Project.all
+    projects.each do |project|
+      if project.deadline == Date.today
+        developer = Developer.find(project.developer_id)
+        @notification = developer.notifications.build(text: "ALARM! Deadline for the project " + @project.title + " is over", read: false)
+        @notification.save
+    end
+  end
     
 end
