2ab17def282cda530c99d04f7cc224c50c4ed03b
Alessandro Brancaleoni <alessan.brancaleoni@edu.unife.it>
2024-02-17 15:19:05 +0100

    Cancellazione codice commentato

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -54,15 +54,6 @@ class ProjectsController < ApplicationController
       end
     end
 
-
-      # if @project.save
-      #  format.html { redirect_to project_url(@project), notice: "Project was successfully created." }
-      #  format.json { render :show, status: :created, location: @project }
-      # else
-      #  format.html { render :new,  status: :unprocessable_entity }
-      #  format.json { render json: @project.errors, status: :unprocessable_entity }
-      # end
-    # end
   end
 
   # PATCH/PUT /projects/1 or /projects/1.json
