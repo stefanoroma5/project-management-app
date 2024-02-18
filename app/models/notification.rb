59a0dc788e0ac8b8b012270ebf521e7b31179a37
stefanoroma5 <stefanoromanini97@gmail.com>
2024-02-19 00:16:40 +0100

    Added notifications model test, validation and scope

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,19 @@
 class Notification < ApplicationRecord
   belongs_to :developer
 
+  validates :text,
+    presence: true
+  validates :read,
+    presence: true,
+    inclusion: {in: [true, false], message: "%{value} is not valid"}
+
+  scope :unread, -> { where(read: false) }
+  scope :read, -> { where(read: true) }
+  scope :recent, ->(*args) {
+    where("created_at > ?",
+      (args.first || 2.weeks.ago))
+  }
+
   def self.check_deadlines
     projects = Project.all
     projects.each do |project|
