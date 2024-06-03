d11ae00dc901a65bb5db219fe5da0dea291fd883
stefanoroma5 <stefanoromanini97@gmail.com>
2024-06-03 16:58:09 +0200

    primi cambiamenti estetici a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,34 +24,31 @@ class TasksController < ApplicationController
   # POST /tasks or /tasks.json
   def create
     @project = Project.find(params[:project_id])
-    # add the task to the project
     @task = @project.tasks.build(task_params)
+    notification = nil
 
-    # Assuming developer_ids is an array of developer ids passed in params
-    if params["developer_emails"].present?
-      developer_emails = params["developer_emails"]
-
-      developer_emails.each do |developer_email|
-        developer = Developer.find_by(email: developer_email)
-        @developer_task = DeveloperTask.new(developer: developer, task: @task)
+    # Add developers to the task
+    if params[:task][:developer_ids].present?
+      developer_ids = params[:task][:developer_ids]
+      developer_ids.each do |developer_id|
+        @task.developer_tasks.build(developer_id: developer_id)
+        developer = Developer.find(developer_id)
+        notification = developer.notifications.build(text: "You have been assigned to the task " + @task.title + " of project " + @project.title, read: false)
       end
     end
 
     respond_to do |format|
-      if params["developer_emails"].present?
-        saved = @task.save && @developer_task.save && @project.save
-        error_object = @developer_task.errors.any? ? @developer_task : @task.errors.any? ? @task : @project
-      else
-        saved = @task.save && @project.save
-        error_object = @task.errors.any? ? @task : @project
-      end
-
-      if saved
+      if @task.save
+        if !notification.nil? && notification.save
+          puts "Notification created"
+        else
+          puts notification.errors.full_messages
+        end
         format.html { redirect_to project_task_url(@project, @task), notice: "Task was successfully created." }
         format.json { render :show, status: :created, location: @task }
       else
-        format.html { render :new, alert: "Unprocessable entity. Errors: #{error_object.errors.full_messages.join(", ")}", status: :unprocessable_entity }
-        format.json { render json: error_object.errors, status: :unprocessable_entity }
+        format.html { render :new, alert: "Unprocessable entity. Errors: #{@task.errors.full_messages.join(", ")}", status: :unprocessable_entity }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -59,9 +56,24 @@ class TasksController < ApplicationController
   # PATCH/PUT /tasks/1 or /tasks/1.json
   def update
     respond_to do |format|
+      # destroy all developers assigned to the task
       if @task.update(task_params)
-        format.html { redirect_to task_url(@task), notice: "Task was successfully updated." }
-        format.json { render :show, status: :ok, location: @task }
+        @task.developer_tasks.destroy_all
+
+        if params[:task][:developer_ids].present?
+          developer_ids = params[:task][:developer_ids]
+          developer_ids.each do |developer_id|
+            @task.developer_tasks.build(developer_id: developer_id)
+          end
+        end
+
+        if @task.save
+          format.html { redirect_to project_task_url(@project, @task), notice: "Task was successfully updated." }
+          format.json { render :show, status: :ok, location: @task }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @task.errors, status: :unprocessable_entity }
+        end
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @task.errors, status: :unprocessable_entity }
@@ -92,6 +104,6 @@ class TasksController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def task_params
-    params.require(:task).permit(:start_date, :end_date, :status, :description, :task_type, :estimation, :priority, :title)
+    params.require(:task).permit(:start_date, :end_date, :status, :description, :task_type, :estimation, :priority, :title, developer_tasks: [])
   end
 end
